2eb6c29aa30030043cf77c39250a4309eec9e191
ArchLeaders <archparliament@gmail.com>
2022-08-25 01:40:41 -0700

    Add project files.

---
+++ ArmorUpgradePhysicsPatcher/Dashboard.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ArmorUpgradePhysicsPatcher
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.TbSourceActor = new System.Windows.Forms.TextBox();
+            this._labelSourceActor = new System.Windows.Forms.Label();
+            this.BtnBrowseSourceActor = new System.Windows.Forms.Button();
+            this.BtnRun = new System.Windows.Forms.Button();
+            this.BtnAuto = new System.Windows.Forms.Button();
+            this._labelUpgradeNumbers = new System.Windows.Forms.Label();
+            this.TbUpgradeIncrements = new System.Windows.Forms.TextBox();
+            this.ToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // TbSourceActor
+            // 
+            this.TbSourceActor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbSourceActor.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TbSourceActor.Location = new System.Drawing.Point(12, 55);
+            this.TbSourceActor.Margin = new System.Windows.Forms.Padding(3, 5, 10, 3);
+            this.TbSourceActor.Name = "TbSourceActor";
+            this.TbSourceActor.Size = new System.Drawing.Size(654, 34);
+            this.TbSourceActor.TabIndex = 0;
+            this.ToolTip.SetToolTip(this.TbSourceActor, "Source ActorPack file");
+            // 
+            // _labelSourceActor
+            // 
+            this._labelSourceActor.AutoSize = true;
+            this._labelSourceActor.Location = new System.Drawing.Point(12, 24);
+            this._labelSourceActor.Margin = new System.Windows.Forms.Padding(3, 15, 3, 0);
+            this._labelSourceActor.Name = "_labelSourceActor";
+            this._labelSourceActor.Size = new System.Drawing.Size(114, 25);
+            this._labelSourceActor.TabIndex = 1;
+            this._labelSourceActor.Text = "Source Actor";
+            // 
+            // BtnBrowseSourceActor
+            // 
+            this.BtnBrowseSourceActor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnBrowseSourceActor.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.BtnBrowseSourceActor.FlatAppearance.BorderSize = 2;
+            this.BtnBrowseSourceActor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.BtnBrowseSourceActor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Navy;
+            this.BtnBrowseSourceActor.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.BtnBrowseSourceActor.Location = new System.Drawing.Point(681, 54);
+            this.BtnBrowseSourceActor.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnBrowseSourceActor.Name = "BtnBrowseSourceActor";
+            this.BtnBrowseSourceActor.Size = new System.Drawing.Size(111, 38);
+            this.BtnBrowseSourceActor.TabIndex = 2;
+            this.BtnBrowseSourceActor.Text = "Browse";
+            this.ToolTip.SetToolTip(this.BtnBrowseSourceActor, "Browse for an sbctorpack file.");
+            this.BtnBrowseSourceActor.UseVisualStyleBackColor = true;
+            this.BtnBrowseSourceActor.Click += new System.EventHandler(this.BtnBrowseSourceActor_Click);
+            // 
+            // BtnRun
+            // 
+            this.BtnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnRun.Location = new System.Drawing.Point(681, 230);
+            this.BtnRun.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnRun.Name = "BtnRun";
+            this.BtnRun.Size = new System.Drawing.Size(111, 38);
+            this.BtnRun.TabIndex = 3;
+            this.BtnRun.Text = "Generate";
+            this.BtnRun.UseVisualStyleBackColor = true;
+            this.BtnRun.Click += new System.EventHandler(this.BtnRun_Click);
+            // 
+            // BtnAuto
+            // 
+            this.BtnAuto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnAuto.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.BtnAuto.FlatAppearance.BorderSize = 2;
+            this.BtnAuto.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.BtnAuto.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Navy;
+            this.BtnAuto.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.BtnAuto.Location = new System.Drawing.Point(681, 131);
+            this.BtnAuto.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnAuto.Name = "BtnAuto";
+            this.BtnAuto.Size = new System.Drawing.Size(111, 38);
+            this.BtnAuto.TabIndex = 6;
+            this.BtnAuto.Text = "Autofill";
+            this.ToolTip.SetToolTip(this.BtnAuto, "Attempts to autofill the upgrade increments from the ActorInfo.");
+            this.BtnAuto.UseVisualStyleBackColor = true;
+            this.BtnAuto.Click += new System.EventHandler(this.BtnAuto_Click);
+            // 
+            // _labelUpgradeNumbers
+            // 
+            this._labelUpgradeNumbers.AutoSize = true;
+            this._labelUpgradeNumbers.Location = new System.Drawing.Point(12, 103);
+            this._labelUpgradeNumbers.Margin = new System.Windows.Forms.Padding(3, 15, 3, 0);
+            this._labelUpgradeNumbers.Name = "_labelUpgradeNumbers";
+            this._labelUpgradeNumbers.Size = new System.Drawing.Size(173, 25);
+            this._labelUpgradeNumbers.TabIndex = 5;
+            this._labelUpgradeNumbers.Text = "Upgrade Increments";
+            // 
+            // TbUpgradeIncrements
+            // 
+            this.TbUpgradeIncrements.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbUpgradeIncrements.Font = new System.Drawing.Font("Segoe UI Emoji", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TbUpgradeIncrements.Location = new System.Drawing.Point(12, 135);
+            this.TbUpgradeIncrements.Margin = new System.Windows.Forms.Padding(3, 5, 10, 3);
+            this.TbUpgradeIncrements.Name = "TbUpgradeIncrements";
+            this.TbUpgradeIncrements.PlaceholderText = "### ; ### ; ###";
+            this.TbUpgradeIncrements.Size = new System.Drawing.Size(654, 34);
+            this.TbUpgradeIncrements.TabIndex = 4;
+            this.ToolTip.SetToolTip(this.TbUpgradeIncrements, "List of armor upgrades to use or create.");
+            this.TbUpgradeIncrements.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbUpgradeIncrements_KeyPress);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 282);
+            this.Controls.Add(this.BtnAuto);
+            this.Controls.Add(this._labelUpgradeNumbers);
+            this.Controls.Add(this.TbUpgradeIncrements);
+            this.Controls.Add(this.BtnRun);
+            this.Controls.Add(this.BtnBrowseSourceActor);
+            this.Controls.Add(this._labelSourceActor);
+            this.Controls.Add(this.TbSourceActor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Dashboard";
+            this.Text = "Botw - Armor Upgrade Physics Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbSourceActor;
+        private System.Windows.Forms.Label _labelSourceActor;
+        private System.Windows.Forms.Button BtnBrowseSourceActor;
+        private System.Windows.Forms.Button BtnRun;
+        private System.Windows.Forms.Button BtnAuto;
+        private System.Windows.Forms.Label _labelUpgradeNumbers;
+        private System.Windows.Forms.TextBox TbUpgradeIncrements;
+        private System.Windows.Forms.ToolTip ToolTip;
+    }
+}
---
+++ ArmorUpgradePhysicsPatcher/Dashboard.cs
@@ -0,0 +1,157 @@
+﻿using Botw.Cryptohraphy;
+using Nintendo.Byml;
+using Nintendo.Sarc;
+using Nintendo.Yaz0;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArmorUpgradePhysicsPatcher
+{
+    public partial class Dashboard : Form
+    {
+        private readonly List<string> keys = new() { "hkcl", "bphysics", "bphyssb" };
+
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowseSourceActor_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new();
+            openFileDialog.Filter = "Actor Pack (*.sbactorpack)|*.sbactorpack";
+            openFileDialog.ShowDialog();
+
+            TbSourceActor.Text = openFileDialog.FileName;
+        }
+
+        private async void BtnAuto_Click(object sender, EventArgs e)
+        {
+            if (TbSourceActor.Text != "") {
+                var ids = await Auto(TbSourceActor.Text);
+                TbUpgradeIncrements.Text = string.Join(" ; ", ids);
+            }
+            else {
+                MessageBox.Show("No actor pack specified.", "Error");
+            }
+        }
+
+        private async void BtnRun_Click(object sender, EventArgs e)
+        {
+            if (TbSourceActor.Text != "" && File.Exists(TbSourceActor.Text)) {
+                await Generate();
+                MessageBox.Show("Task completed!", "Notice");
+            }
+            else {
+                MessageBox.Show("No actor pack specified.", "Error");
+            }
+        }
+
+        private void TbUpgradeIncrements_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ' || e.KeyChar == ';' || e.KeyChar == (char)Keys.Enter) {
+                if (TbUpgradeIncrements.Text != "" && TbUpgradeIncrements.Text.Last() != ' ') {
+                    TbUpgradeIncrements.Text += " ; ";
+                    int len = TbUpgradeIncrements.Text.Length;
+                    TbUpgradeIncrements.Select(len, len);
+                }
+                e.Handled = true;
+            }
+            else if (e.KeyChar == (char)Keys.Back) {
+                if (TbUpgradeIncrements.Text != "" && TbUpgradeIncrements.Text.Last() == ' ') {
+                    int len = TbUpgradeIncrements.Text.Length - 1;
+                    TbUpgradeIncrements.Text = TbUpgradeIncrements.Text[0..(len - 2)];
+                    TbUpgradeIncrements.Select(TbUpgradeIncrements.Text.Length, TbUpgradeIncrements.Text.Length);
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private static Task<List<string>> Auto(string actorpack)
+        {
+            string actorinfo = $"{actorpack}\\..\\..\\ActorInfo.product.sbyml -- nope";
+
+            if (!File.Exists(actorinfo)) {
+                var bcml = new Bcml.Utils.Settings();
+                actorinfo = $"{bcml.GetUpdate()}\\Actor\\ActorInfo.product.sbyml";
+            }
+
+            BymlFile byml = new(Yaz0.DecompressFast(actorinfo));
+            BymlNode baseActor = new(new Dictionary<string, BymlNode>());
+            Dictionary<string, BymlNode> hashedArray = new();
+            var actorname = Path.GetFileNameWithoutExtension(actorpack);
+
+            foreach (var actor in byml.RootNode.Hash["Actors"].Array) {
+                string name = actor.Hash["name"].String;
+                hashedArray.Add(CRC32.ComputeHash(actor.Hash["name"].String), actor);
+
+                if (name == actorname) {
+                    baseActor = actor;
+                }
+            }
+
+            List<string> ids = new() { baseActor.Hash["name"].String.Split("_")[1] };
+            while (baseActor.Hash.ContainsKey("armorNextRankName") && baseActor.Hash["armorNextRankName"].String != "") {
+                string nextActorname = baseActor.Hash["armorNextRankName"].String;
+                ids.Add(nextActorname.Split("_")[1]);
+                baseActor = hashedArray[CRC32.ComputeHash(nextActorname)];
+            }
+
+            return Task.FromResult(ids);
+        }
+
+        private Task Generate()
+        {
+            SarcFile sarc = new(Yaz0.DecompressFast(File.ReadAllBytes(TbSourceActor.Text)));
+
+            var baseFiles = GetFiles(sarc.Files);
+            var type = Path.GetFileNameWithoutExtension(TbSourceActor.Text).Split("_")[2];
+            var bcml = new Bcml.Utils.Settings();
+            foreach (var id in TbUpgradeIncrements.Text.Split(" ; ")) {
+                var actorpackFile = $"{bcml.GetUpdate()}\\Actor\\Pack\\Armor_{id}_{type}.sbactorpack";
+                if (File.Exists(actorpackFile)) {
+                    SarcFile actorpack = new(Yaz0.DecompressFast(actorpackFile));
+                    var files = GetFiles(actorpack.Files);
+
+                    foreach (var key in keys) {
+                        if (baseFiles.ContainsKey(key)) {
+                            if (key == "bphysics") {
+                                actorpack.Files[files[key].Key] = baseFiles[key].Value;
+                            }
+                            else {
+                                if (files.ContainsKey(key)) {
+                                    actorpack.Files.Remove(files[key].Key);
+                                }
+                                actorpack.Files.Add(baseFiles[key].Key, baseFiles[key].Value);
+                            }
+                        }
+                    }
+
+                    File.WriteAllBytes($"{Path.GetDirectoryName(TbSourceActor.Text)}\\Armor_{id}_{type}.sbactorpack", Yaz0.CompressFast(actorpack.ToBinary()));
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Dictionary<string, KeyValuePair<string, byte[]>> GetFiles(Dictionary<string, byte[]> files)
+        {
+            Dictionary<string, KeyValuePair<string, byte[]>> result = new();
+
+            foreach (var file in files)
+                foreach (var key in keys)
+                    if (file.Key.EndsWith(key))
+                        result.Add(key, file);
+
+            return result;
+        }
+    }
+}
